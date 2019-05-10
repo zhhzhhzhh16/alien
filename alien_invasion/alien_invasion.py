ddf8a2124514d880f91a970032ad0bd38a7cf48c
zhhzhhzhh16 <50470224+zhhzhhzhh16@users.noreply.github.com>
2019-05-10 09:42:10 +0800

    Add files via upload

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # 初始化游戏、设置、屏幕对象
+    pygame.init()   
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    # 开始游戏的主循环
+    while True:
+
+        # 监视键盘和鼠标事件
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+        # 每次循环时都重绘屏幕
+        gf.update_screen(ai_settings, screen, ship)
+        
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+
+run_game()
